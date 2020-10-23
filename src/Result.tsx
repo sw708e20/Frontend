cc9d8ce14e9fa49f83279bae0a74be9666d8c2e9
Jonas Krogh <36476478+Badgie@users.noreply.github.com>
2020-10-23 12:48:20 +0200

    integrate api calls and prettify page layout (#4)

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,48 +1,50 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import axios from "axios";
 
 interface EducationType {
+    readonly id: number;
+    readonly education: number;
     readonly name: string;
     readonly url: string;
 }
 
 interface Education {
+    readonly id: number;
     readonly name: string;
     readonly description: string;
-    readonly types: EducationType[];
+    readonly education_types: EducationType[];
 }
 
 class ResultPage extends React.Component {
-    getEducations() {
-        return (
-            [
-                {name: 'God uddannelse', description: 'den er bare nice',
-                    types: [{name: 'bachelor', url: 'yeet.dk'}, {name: 'masters', url: 'yeet.dk'}]},
-                {name: 'Rigtig god uddannelse', description: 'mere nice',
-                    types: [{name: 'bachelor', url: 'yeet.dk'}, {name: 'masters', url: 'yeet.dk'}]},
-                {name: 'Et eller andet humaniora', description: ':nauseous:',
-                    types: [{name: 'EUD', url: ':nauseous:.dk'}]}
-            ]
-        )
-    }
+    state = {list: [], loading: true};
 
+    async getEducations() {
+        if (this.state.loading) {
+            let res = await axios.post(`http://edufinder.dk/recommend/`,
+                {this_should: 'be questions and answers'});
+            let obj = res.data;
+            this.setState({list: obj, loading: false});
+        }
+    }
     renderTitle() {
         return (
-            <h1> {'Recommended educations 4 u'} </h1>
+            <h1 className={'title'}> {'Recommended educations 4 u'} </h1>
         )
     }
 
     renderEducations() {
         const elems:React.ReactElement[] = [];
 
-        for (let edu of this.getEducations()) {
+        for (let edu2 of this.state.list) {
             const eduTypes:React.ReactElement[] = [];
+            let edu:Education = edu2;
 
-            for (let eduType of edu.types) {
+            for (let eduType of edu.education_types) {
                 eduTypes.push(
-                    <div className={'row justify-content-center'}>
-                        <div className={'col-4'}> {eduType.name} </div>
-                        <div className={'col-8'}><a href={eduType.url}> {eduType.url} </a></div>
+                    <div key={eduType.id} className={'row justify-content-center'}>
+                        <div className={'col-6'}><p> {eduType.name} </p></div>
+                        <div className={'col-6'}><a href={eduType.url}> Link to www.ug.dk </a></div>
                     </div>
                 )
             }
@@ -52,27 +54,30 @@ class ResultPage extends React.Component {
                     <div className={'row justify-content-center'}>
                         <h3 className={'education-header'}> {edu.name} </h3>
                     </div>
+                    <hr/>
                     <div className={'row justify-content-center'}>
-                        <p> {edu.description} </p>
+                        <div className={'col-10'}> {edu.description} </div>
                     </div>
+                    <hr/>
                     {eduTypes}
                 </div>
             )
         }
-
+        console.log(elems);
         return (
             <div> {elems} </div>
         )
     }
 
     render() {
+        this.getEducations();
         return (
             <div>
-                <div className={'row'}>
+                <div className={'row justify-content-center'}>
                     {this.renderTitle()}
                 </div>
                 <div className={'row justify-content-center'}>
-                    {this.renderEducations()}
+                    {this.state.loading ? '' : this.renderEducations()}
                 </div>
             </div>
         )
