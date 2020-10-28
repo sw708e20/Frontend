e7863322374d807104c13752be9526523b663c6b
Thorulf Neustrup <thorulf@live.dk>
2020-10-28 11:14:13 +0100

    Next question support for quiz page (#7)
    * Updated QuestionManager to fit server api
    
    * Added get next question support for quiz page
    
    * Removed unnessecary imports

--- src/Recommender.tsx
+++ src/Recommender.tsx
@@ -2,17 +2,16 @@ import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
-import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
-import ToggleButton from "react-bootstrap/ToggleButton";
 import Result from "./Result";
 import ReactDOM from "react-dom";
-import { Question, questionManager, Answer_Enum, getAnswerString } from "./QuestionManager";
+import { Question, questionManager, Answer_Enum, getAnswerString, Answer } from "./QuestionManager";
 
 interface IRecommenderProps {
 
 }
 
 interface IRecommenderState {
+    answers: Array<Answer>
     question?: Question;
 }
 
@@ -23,34 +22,32 @@ class Page extends React.Component<IRecommenderProps, IRecommenderState> {
 
     constructor(props:any) {
         super(props);
-        this.state = {question: undefined};
+        
+        this.state = {answers: [] ,question: undefined};
     }
     
     componentDidMount() {
         questionManager.getFirstQuestion().then((qst) => {
             this.setState({
+                answers: this.state.answers,
                 question: qst
             })
         });
-        
     }
 
     renderTitle() {
         return (
-            <h1 className={'title'}> {this.state.question? this.state.question.question: "null"} </h1>
+            <h1 className={'title'}>Q{this.state.answers.length + 1}: {this.state.question? this.state.question.question: "null"} </h1>
         )
     }
 
     renderAnswerOptions() {
-        let answerOptions:string[] = ['Yes', 'No', 'Probably', 'Probably not', 'Dont know']
         const elems:React.ReactElement[] = [];
 
         for (let answer of this.answer_options) {
             let stringvalue = getAnswerString(answer);
             elems.push(
-                <ToggleButtonGroup type={'radio'} value={answerOptions} name={'answer'} className={'div-spacing'}>
-                    <ToggleButton className={'answer-choice edu-btn'} value={answer}> {stringvalue} </ToggleButton>
-                </ToggleButtonGroup>
+                <button onClick={() => this.onAnswerGiven(answer)} className={'btn btn-primary next-btn edu-btn div-spacing'}> {stringvalue} </button>
             )
             elems.push(<br/>)
         }
@@ -62,17 +59,37 @@ class Page extends React.Component<IRecommenderProps, IRecommenderState> {
         )
     }
 
-    renderNextButton(text: string) {
-        return (
-            <button className={"btn btn-primary next-btn edu-btn"} onClick={() =>
-                ReactDOM.render(
-                    <React.StrictMode>
-                        <Result />
-                    </React.StrictMode>,
-                    document.getElementById('root')
-                )
-            }> {text} </button>
-        )
+    onAnswerGiven(answer_value : Answer_Enum){
+        if(this.state.question === undefined){
+            return;
+        }
+        
+        let new_answer = new Answer(this.state.question, answer_value)
+        
+        let answers = this.state.answers.concat([new_answer])
+        
+        if(answers.length >= 20){
+            this.finishQuiz()
+        }else{
+            this.getNextQuestion(answers)
+        }
+    }
+
+    getNextQuestion(answers: Answer[]){
+        questionManager.getQuestion(answers).then((qst)=>{
+            this.setState({
+                answers: answers,
+                question: qst
+            })
+        });
+    }
+
+    finishQuiz() {
+        ReactDOM.render(
+            <React.StrictMode>
+                <Result />
+            </React.StrictMode>,
+            document.getElementById('root'))
     }
 
     render() {
@@ -83,9 +100,6 @@ class Page extends React.Component<IRecommenderProps, IRecommenderState> {
                 </div>
                 <div className={'row justify-content-center'}>
                     {this.renderAnswerOptions()}
-                </div>
-                <div className={'row justify-content-center'}>
-                    {this.renderNextButton('Next')}
                 </div>
             </div>
 
