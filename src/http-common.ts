65362904a7d68e89f945b6c2d19b18e337637523
Jonas Andersen <jonas@anrsn.me>
2020-10-30 13:36:11 +0100

    Move API to SSL (#14)

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://" + process.env.REACT_APP_DOMAIN,
+  baseURL: process.env.REACT_APP_DOMAIN,
   headers: {
     "Content-Type": "application/json"
   }
