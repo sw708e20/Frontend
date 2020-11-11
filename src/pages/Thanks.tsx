4e758db4c6893685a2e5641e3fbf4bb97c3a8389
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2020-11-11 10:49:12 +0100

    Finalized and cleaned up thanks page

--- src/pages/Thanks.tsx
+++ src/pages/Thanks.tsx
@@ -1,28 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Translation } from "react-i18next";
 import React, { ReactNode } from 'react';
-import {resultPageCommon} from './commons/ResultPageCommon'
-import { questionManager, Answer, Education} from "../services/QuestionManager";
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 class ThanksPage extends React.Component<RouteComponentProps> {
 
     constructor(props:any) {
         super(props);
-
-        this.state = {
-            answers: this.props.location.state as Answer[],
-            loading: true,
-            list: []
-        };
     }
-
-    componentDidMount() : void{
-        this.setState({
-            loading: true
-        })
-    }
-
 
     renderTitle(text_key: string) : ReactNode {
         return (
@@ -48,26 +33,13 @@ class ThanksPage extends React.Component<RouteComponentProps> {
         )
     }
 
-    render() {
+    render() : ReactNode {
         return (
-            
-            <div>
-                <div className={'row justify-content-center'}>
-                    {this.renderTitle('thanks.thanks_title')}
-                </div>
-                <hr/>
-                <div className={'row justify-content-center'}>
-                    <div className={'primary-edu-block div-spacing'}>
-                    <div>
-                <hr/>
-                <div className={'row justify-content-center'}>
-                    <div className={'col-10'}> {this.renderContent('thanks.thanks_content')} </div>
-                </div>
-            </div>
+                <div>
+                    <div>{this.renderTitle('thanks.thanks_title')}</div>
                     <hr/>
+                    <div>{this.renderContent('thanks.thanks_content')}</div>
                 </div>
-                </div>
-            </div>
         )
     }
 }
